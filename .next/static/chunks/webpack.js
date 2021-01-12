6c388c5f904ae421fd1c29279ed3d30494ef72ce
clins <klaulins@gmail.com>
2021-01-12 20:39:43 +0000

    add gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a7e6d27d829f200c1d98";
+/******/ 	var hotCurrentHash = "d11e7f632591b0ae0527";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
